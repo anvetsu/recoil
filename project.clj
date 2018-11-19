2c150af0793cc8c199118433c9185bc97e85498b
Vijay Mathew <mathew.vijay@gmail.com>
2018-11-19 10:47:04 +0530

    Project file added.

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject recoil "0.1.0-SNAPSHOT"
+  :description "A resilience and fault-handling library for Clojure."
+  :url "https://anvetsu.com"
+  :dependencies [[org.clojure/clojure "1.9.0"]])
