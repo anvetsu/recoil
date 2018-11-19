2b42b1e03bdb5e72038754c8128e13b4a89c2584
Vijay Mathew <mathew.vijay@gmail.com>
2018-11-19 16:17:55 +0530

    Tests - first commit

---
+++ test/recoil/retry_test.clj
@@ -0,0 +1,39 @@
+(ns recoil.retry-test
+  (:require [recoil.retry :as r])
+  (:use clojure.test)
+  (:import [java.util.concurrent TimeoutException]
+           [java.sql SQLException]))
+
+(deftest test-make
+  (is (fn? (r/executor {}))))
+
+(defn- make-db-connector
+  []
+  ;; Simulates a database connection with exceptions.
+  (let [state (atom 0)]
+    (fn []
+      (cond
+        (= @state 0) (do
+                       (reset! state 1)
+                       (throw (TimeoutException.)))
+        (= @state 1) (do
+                       (reset! state 2)
+                       (throw (SQLException.)))
+        :else {:ok :connected}))))
+
+(deftest test-execptions
+  (let [exec (r/executor {:handle [TimeoutException SQLException]
+                          :retry 3})]
+    (is (= (exec (make-db-connector)) {:ok :connected})))
+  (let [exec (r/executor {:handle [TimeoutException]
+                          :retry 3})]
+    (try
+      (do (exec (make-db-connector))
+          (is false))
+      (catch SQLException ex
+        (is true)))))
+
+(deftest test-no-retries
+  (let [exec (r/executor {:handle [TimeoutException SQLException]
+                          :retry 1})]
+    (is (= (exec (make-db-connector)) {:status :no-retries}))))
