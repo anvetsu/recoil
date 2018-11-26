be226f2b6aa849bbd7dd71bcf1782fbaa35804ff
Vijay Mathew <mathew.vijay@gmail.com>
2018-11-26 16:41:22 +0530

    core.async dependency added

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject recoil "0.1.0-SNAPSHOT"
   :description "A resilience and fault-handling library for Clojure."
   :url "https://anvetsu.com"
-  :dependencies [[org.clojure/clojure "1.9.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/core.async "0.4.490"]])
